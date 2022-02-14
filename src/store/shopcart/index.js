77c3a0d0aec04737050213ecd52fa1471543afcf
larry <492178939@qq.com>
2022-02-14 16:41:48 +0800

    购物车展示

--- src/store/shopcart/index.js
+++ src/store/shopcart/index.js
@@ -1,15 +1,28 @@
 import {reqCarList} from '@/api';
 
-const state = {};
-const mutations = {};
+const state = {
+  cartList: []
+};
+const mutations = {
+  GERCARTLIST(state, cartList) {
+    state.cartList = cartList
+  }
+};
 const actions = {
   //获取购物车列表数据
   async getCartList({commit}) {
     let result = await reqCarList();
     console.log(result);
+    if (result.code == 200) {
+      commit('GERCARTLIST', result.data)
+    }
   }
 };
-const getters = {};
+const getters = {
+  cartList(state) {
+    return state.cartList[0] || {}
+  },
+};
 export default {
   state,
   mutations,
